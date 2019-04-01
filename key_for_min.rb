d0857743c3ec73b07d2d2f7e48eaae3be5804532
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-04-01 00:25:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,24 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.empty?
+    return nil
+  end
+
+  smallest_value = nil
+  smallest_key = :symbol
+
+  name_hash.each do |key, value|
+    smallest_value = value if smallest_value == nil
+    smallest_value = value if value < smallest_value
+  end
+
+  name_hash.each do |key, value|
+    if value == smallest_value
+      smallest_key = key
+      break
+    end
+  end
+
+  smallest_key
 end
